071af4369e09357f1e1d1667edfb6b9f527b8b24
Mert Balkan <mert__balkan@hotmail.com>
2021-06-24 15:57:19 +0300

    Enemy Added

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPSGame.Concretes.Controllers
+{
+    public class EnemyController : MonoBehaviour
+    {
+
+    }
+}
