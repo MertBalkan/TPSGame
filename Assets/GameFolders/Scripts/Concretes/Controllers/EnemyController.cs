7111d8d81c3a91f6d63bf3c748d6ec2105d9f25b
Mert Balkan <mert__balkan@hotmail.com>
2021-06-28 13:31:16 +0300

    Enemy attack scripts finished

--- Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
+++ Assets/GameFolders/Scripts/Concretes/Controllers/EnemyController.cs
@@ -18,6 +18,9 @@ namespace TPSGame.Concretes.Controllers
         private IHealth _health;
         private CharacterAnimation _animation;
         private NavMeshAgent _navMeshAgent;
+        private InventoryController _inventoryController;
+        private Transform _playerTransform;
+        private bool _canAttack;
 
         private void Awake()
         {
@@ -25,17 +28,30 @@ namespace TPSGame.Concretes.Controllers
             _mover = new MoveWithNavMesh(this);
             _animation = new CharacterAnimation(this);
             _health = GetComponent<IHealth>();
+            _inventoryController = GetComponent<InventoryController>();
         }
-
+        private void Start()
+        {
+            _playerTransform = FindObjectOfType<PlayerController>().transform;
+        }
         private void Update()
         {
             if (_health.IsDead) return;
-            _mover.MoveAction(_playerPrefab.transform.position, 10f);
+            _mover.MoveAction(_playerTransform.position, 10f);
+            _canAttack = Vector3.Distance(_playerTransform.position, this.transform.position) <= _navMeshAgent.stoppingDistance && _navMeshAgent.velocity == Vector3.zero;
+            Debug.Log(_canAttack);
         }
-
+        private void FixedUpdate()
+        {
+            if (_canAttack)
+            {
+                _inventoryController.CurrentWeapon.Attack();
+            }
+        }
         private void LateUpdate()
         {
             _animation.MoveAnimation(_navMeshAgent.velocity.magnitude);
+            _animation.AttackAnimation(_canAttack);
         }
     }
 }
