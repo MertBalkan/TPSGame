adb920c8133e0bd62f89dda90bfaae33d61beec3
Mert Balkan <mert__balkan@hotmail.com>
2021-07-02 14:55:38 +0300

    menu scene and game manager

---
+++ Assets/GameFolders/Scripts/Concretes/Managers/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TPSGame.Abstracts.Utilities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TPSGame.Concretes.Managers
+{
+    public class GameManager : SingletonMonoBehaviour<GameManager>
+    {
+        private void Awake()
+        {
+            SingletonMethod(this);
+        }
+
+        public void LoadLevel(string levelName)
+        {
+            StartCoroutine(LoadLevelAsync(levelName));
+        }
+        private IEnumerator LoadLevelAsync(string levelName)
+        {
+            yield return SceneManager.LoadSceneAsync(levelName);
+        }
+    }
+}
---
+++ Assets/GameFolders/Scripts/Concretes/UIs/StartButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TPSGame.Concretes.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TPSGame.Concretes.UIs
+{
+    public class StartButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            _button = GetComponent<Button>();
+        }
+        private void OnEnable()
+        {
+            _button.onClick.AddListener(HandleOnButtonClicked);
+        }
+        private void OnDisable()
+        {
+            _button.onClick.RemoveListener(HandleOnButtonClicked);
+        }
+        private void HandleOnButtonClicked()
+        {
+            GameManager.Instance.LoadLevel("Game");
+        }
+    }
+}
