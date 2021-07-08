0fcd3d619c8113503218324dfb635d8f5c94f890
Mert Balkan <mert__balkan@hotmail.com>
2021-07-08 14:14:04 +0300

    sounds added

---
+++ Assets/GameFolders/Scripts/Concretes/Controllers/SoundController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TPSGame.Concretes.Controllers
+{
+    public class SoundController : MonoBehaviour
+    {
+        private AudioSource _audioSource;
+
+        private void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        public void SetClip(AudioClip clip)
+        {
+            _audioSource.clip = clip;
+        }
+
+        public void PlaySound()
+        {
+            _audioSource.Play();
+        }
+    }
+}
---
+++ Assets/GameFolders/Scripts/Concretes/Managers/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TPSGame.Abstracts.Utilities;
+using TPSGame.Concretes.Controllers;
+using UnityEngine;
+
+namespace TPSGame.Concretes.Managers
+{
+    public class SoundManager : SingletonMonoBehaviour<SoundManager>
+    {
+        [SerializeField] private AudioClip[] _clips;
+        private SoundController[] _soundControllers;
+
+
+        private void Awake()
+        {
+            SingletonMethod(this);
+            _soundControllers = GetComponentsInChildren<SoundController>();
+        }
+
+        private void Start()
+        {
+            _soundControllers[0].SetClip(_clips[0]);
+            _soundControllers[1].SetClip(_clips[1]);
+
+            _soundControllers[0].PlaySound();
+        }
+
+        public void PlayMachineGun(Vector3 position){
+
+        }
+    }
+}
